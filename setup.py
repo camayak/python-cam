3123c4d31b06a6985fa4af297d61d35987ad0ca5
Peter Evans <evans.peter@gmail.com>
2019-10-15 15:45:37 +0100

    Adding setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PythonCam',
+    version='0.1dev',
+    packages=['cam'],
+    author='Peter Evans',
+    author_email='peter@camayak.com',
+    url='https://justask.cam',
+    license='GNU General Public License v3.0',
+    long_description=open('README.md').read(),
+)
